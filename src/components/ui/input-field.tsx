9d7347e77f8f3e79d7bfe2206f4186da8f963d9d
Grzegorz Dubiel <greg2012201@gmail.com>
2024-12-08 00:42:17 +0100

    Create the input file component.

---
+++ src/components/ui/input-field.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEvent, InputHTMLAttributes } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface InputFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> {
+    label?: string;
+    labelClassName?: string;
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+function InputField({
+    label,
+    id,
+    name,
+    className,
+    labelClassName,
+    onChange,
+    accept = ".jpg,.jpeg,.png,.gif",
+    multiple,
+    ...rest
+}: InputFieldProps) {
+    return (
+        <div>
+            {label && (
+                <Label htmlFor={id} className={`text-sm ${labelClassName || ""}`}>
+                    {label}
+                </Label>
+            )}
+            <Input
+                id={id}
+                name={name}
+                type="file"
+                onChange={onChange}
+                className={`max-w-[400px] ${className || ""}`}
+                accept={accept}
+                multiple={multiple}
+                max={2}
+                {...rest}
+            />
+        </div>
+    );
+}
+
+export default InputField;
