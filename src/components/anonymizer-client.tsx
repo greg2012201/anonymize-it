a79c1564a33685d6bf403ad24752ba145d3d225c
Grzegorz Dubiel <greg2012201@gmail.com>
2025-01-28 16:34:32 +0100

    Make anonymizer client handle multiple images.

--- src/components/anonymizer-client.tsx
+++ src/components/anonymizer-client.tsx
@@ -1,42 +1,46 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback } from "react";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
 import useLoadModels from "@/hooks/use-load-models";
 import useFace from "@/hooks/use-face";
+import ImageUploader from "./image-uploader";
 
 function AnonymizerClient() {
-    const [uploadedImage, setUploadedImage] = useState<string | null>();
-
     useLoadModels();
-    const { blurredImage, error, handleFace, isLoading, outputImageContainerRef } = useFace(uploadedImage ?? null);
-    const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        const file = e.target.files?.[0];
-        console.log("file", e.target);
-        if (file) {
-            const reader = new FileReader();
-            // reader.onloadstart = () => setIsLoading(true);
-            reader.onload = (e) => {
-                const dataUrl = e.target?.result as string;
-                setUploadedImage(dataUrl);
-            };
-            reader.readAsDataURL(file);
-        }
-    };
+    const { blurredImages, handleFace, loadExampleImage, loadTargetImages, isLoading } = useFace();
 
     const handleProcess = () => {
-        console.log("begin");
         handleFace();
     };
+
+    const handleOnImageUpload = useCallback(
+        (data: string[]) => {
+            loadTargetImages((prevData) => [...prevData, ...data]);
+        },
+        [loadTargetImages]
+    );
+
     return (
         <div className="flex flex-col w-full max-w-[800px] items-center gap-1.5">
-            <Input className="max-w-[400px]" onChange={handleFileUpload} id="file" name="file" type="file" />
-            {/*  <Button type="submit">Send</Button> */}
+            <div className="flex space-x-4">
+                {" "}
+                <ImageUploader
+                    id="example-face-uploader"
+                    label="image with the example faces"
+                    onImageUpload={(img) => loadExampleImage(img[0])}
+                />
+                <ImageUploader
+                    id="to-anonymize"
+                    allowMultipleFiles
+                    label="images that should be anonymized"
+                    onImageUpload={handleOnImageUpload}
+                />
+            </div>{" "}
             <div className="flex [&>img]:w-[400px] space-x-4">
-                {uploadedImage && <img ref={outputImageContainerRef} src={uploadedImage} />}
-                <img src={blurredImage} />
+                {blurredImages.map((blurredImage) => (
+                    <img key={blurredImage} src={blurredImage} />
+                ))}
             </div>
             {isLoading && <span>Loading...</span>}
             <Button type="button" onClick={handleProcess} disabled={isLoading}>
