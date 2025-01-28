3ffdc44dcaf2cd3869aad891986dcbc577ca6710
Grzegorz Dubiel <greg2012201@gmail.com>
2025-01-28 23:05:07 +0100

    Fix the logic for matching multiple faces.

--- src/hooks/use-face.ts
+++ src/hooks/use-face.ts
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 import * as faceapi from "face-api.js";
 
+type ImageWithDescriptor = {
+  id: number;
+  descriptors: Float32Array<ArrayBufferLike>[];
+  detections: faceapi.WithFaceDescriptor<
+    faceapi.WithFaceLandmarks<
+      {
+        detection: faceapi.FaceDetection;
+      },
+      faceapi.FaceLandmarks68
+    >
+  >[];
+};
+
 async function base64ToImageElement(
   base64Image: string,
 ): Promise<HTMLImageElement> {
@@ -13,17 +26,32 @@ async function base64ToImageElement(
 }
 
 function compareImages(
-  exampleDescriptor: Float32Array,
-  targetDescriptors: Float32Array[],
+  exampleDescriptors: Float32Array[],
+  targetImagesWithDescriptors: ImageWithDescriptor[],
 ) {
-  const threshold = 0.6;
-  return targetDescriptors.filter((targetDescriptor) => {
-    const distance = faceapi.euclideanDistance(
-      exampleDescriptor,
-      targetDescriptor,
-    );
-    return distance < threshold;
+  const threshold = 0.5;
+
+  // try to for each loop and build in detail
+  const matchedImagesWithDescriptors: ImageWithDescriptor[] = [];
+  targetImagesWithDescriptors.forEach(({ detections, ...rest }) => {
+    const matchedDescriptors = detections.filter(({ descriptor }) => {
+      return exampleDescriptors.some((exampleDescriptor) => {
+        const distance = faceapi.euclideanDistance(
+          exampleDescriptor,
+          descriptor,
+        );
+        return distance < threshold;
+      });
+    });
+
+    if (matchedDescriptors.length) {
+      matchedImagesWithDescriptors.push({
+        detections: matchedDescriptors,
+        ...rest,
+      });
+    }
   });
+  return matchedImagesWithDescriptors;
 }
 
 function extractExampleFace(image: HTMLImageElement) {
@@ -31,6 +59,17 @@ function extractExampleFace(image: HTMLImageElement) {
     .detectSingleFace(image)
     .withFaceLandmarks()
     .withFaceDescriptor();
+}
+
+async function extractAllFaces(image: HTMLImageElement) {
+  const detections = await faceapi
+    .detectAllFaces(image)
+    .withFaceLandmarks()
+    .withAgeAndGender()
+    .withFaceExpressions()
+    .withFaceDescriptors();
+
+  return detections;
 }
 
 function useFace() {
@@ -63,7 +102,8 @@ function useFace() {
       const exampleImageElement = await base64ToImageElement(exampleImage);
 
       const exampleFace = await extractExampleFace(exampleImageElement);
-
+      const allFaces = await extractAllFaces(exampleImageElement);
+      console.log("allFaces", allFaces);
       if (!exampleFace) {
         setError("No face detected in the example image");
         return;
@@ -73,57 +113,54 @@ function useFace() {
       const ctx = canvas.getContext("2d")!;
 
       const updatedTargetImages: string[] = [];
-
-      const targetDescriptors = await Promise.all(
-        targetImages.map(async (targetImage) => {
+      const targetImagesWithId = targetImages.map((targetImage, index) => ({
+        id: index,
+        src: targetImage,
+      }));
+      const targetImagesWithDescriptors = await Promise.all(
+        targetImagesWithId.map(async ({ id, src }) => {
           const imgElement = new Image();
-          imgElement.src = targetImage;
+          imgElement.src = src;
           await imgElement.decode();
           const detections = await faceapi
             .detectAllFaces(imgElement)
             .withFaceLandmarks()
             .withFaceDescriptors();
-          return detections.map((detection) => detection.descriptor);
+
+          return {
+            id,
+            detections,
+            descriptors: detections.map((detection) => detection.descriptor),
+            src,
+            imgElement,
+          };
         }),
       );
-      const flattenedDescriptors = targetDescriptors.flat();
 
-      const matchedDescriptors = compareImages(
-        exampleFace.descriptor,
-        flattenedDescriptors,
+      const matchedImagesWithDescriptors = compareImages(
+        allFaces.map((face) => face.descriptor),
+        targetImagesWithDescriptors,
       );
 
-      const matchingImageUrls = targetImages.filter((_, index) => {
-        const descriptorsForImage = targetDescriptors[index];
-        return descriptorsForImage.some((descriptor) =>
-          matchedDescriptors.some(
-            (matchedDescriptor) => matchedDescriptor === descriptor,
-          ),
-        );
-      });
-      if (matchedDescriptors.length > 0) {
-        setMatchingTargetImages((prev) => [...prev, ...targetImages]);
-      }
-      for (const targetImage of matchingImageUrls) {
-        const targetImgElement = new Image();
-        targetImgElement.src = targetImage;
-        await targetImgElement.decode();
+      console.log("matchedDescriptors", matchedImagesWithDescriptors);
 
-        const detections = await faceapi
-          .detectAllFaces(targetImgElement)
-          .withFaceLandmarks()
-          .withFaceDescriptors();
+      for (const targetImage of targetImagesWithDescriptors) {
+        const detections =
+          matchedImagesWithDescriptors.find(
+            (matchedImageWithDescriptors) =>
+              matchedImageWithDescriptors.id === targetImage.id,
+          )?.detections || [];
 
-        if (!detections.length) {
+        if (!detections?.length) {
           continue;
         }
-
+        const targetImgElement = targetImage.imgElement;
         canvas.width = targetImgElement.naturalWidth;
         canvas.height = targetImgElement.naturalHeight;
         ctx.drawImage(targetImgElement, 0, 0, canvas.width, canvas.height);
 
         for (const detection of detections) {
-          const { x, y, width, height } = detection.detection.box;
+          const { x, y, width, height } = detection?.detection?.box;
           ctx.filter = "blur(60px)";
           ctx.drawImage(
             targetImgElement,
