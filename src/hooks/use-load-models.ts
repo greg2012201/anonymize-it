5dbecaff16f4b3e98944bce095de3531401a5621
greg2012201 <greg2012201@gmail.com>
2025-10-07 17:32:21 +0200

    Add the hook for loading loading models.

---
+++ src/hooks/use-load-models.ts
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import * as faceapi from "face-api.js";
+
+const MODEL_PATH = `/models`;
+
+async function loadModels(onError: (err: any) => void) {
+  try {
+    await Promise.all([
+      faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_PATH),
+      faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_PATH),
+      faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_PATH),
+      faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_PATH),
+      faceapi.nets.ageGenderNet.loadFromUri(MODEL_PATH),
+      faceapi.nets.faceExpressionNet.loadFromUri(MODEL_PATH),
+    ]);
+    console.log("models loaded");
+  } catch (err) {
+    onError("Failed to load face detection models");
+    console.error(err);
+  }
+}
+
+function useLoadModels() {
+  useEffect(() => {
+    loadModels((e) => console.log("Error Loading Models", e));
+  }, []);
+}
+
+export default useLoadModels;
